d835770d94b9d6efac2b07ac14167550f64f79a0
yatinvarma <151534004+yatinvarma@users.noreply.github.com>
2023-11-20 23:28:04 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,56 +1,56 @@
 function saveUserForm(event) {
-    event.preventDefault();
+        event.preventDefault();
 
-    const Name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const password = document.getElementById('password').value;
-    const dob = document.getElementById('dob').value;
-    const acceptTerms = document.getElementById('acceptTerms').checked;
+        const Name = document.getElementById('name').value;
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+        const dob = document.getElementById('dob').value;
+        const acceptTerms = document.getElementById('acceptTerms').checked;
 
-    var currentYear = new Date().getFullYear();
-    var birthYear = dob.split("-");
-    let year = birthYear[0];
-    var age = currentYear - year;
+        var currentYear = new Date().getFullYear();
+        var birthYear = dob.split("-");
+        let year = birthYear[0];
+        var age = currentYear - year;
 
-    if (age < 18 || age > 55) {
-        document.getElementById('dob').style.border = '1px solid red';
-        alert("Age must be between 18 and 55");
-    } else {
-        document.getElementById('dob').style.border = 'none';
+        if (age < 18 || age > 55) {
+            document.getElementById('dob').style.border = '1px solid red';
+            alert("Age must be between 18 and 55");
+        } else {
+            document.getElementById('dob').style.border = 'none';
 
-        const entry = {
-            Name,
-            email,
-            password,
-            dob,
-            acceptTerms
-        };
+            const entry = {
+                Name,
+                email,
+                password,
+                dob,
+                acceptTerms
+            };
 
-        let entries = JSON.parse(localStorage.getItem('userEntries')) || [];
-        entries.push(entry);
+            let entries = JSON.parse(localStorage.getItem('userEntries')) || [];
+            entries.push(entry);
 
-        localStorage.setItem('userEntries', JSON.stringify(entries));
-        displayEntries();
-        document.getElementById('user_form').reset();
+            localStorage.setItem('userEntries', JSON.stringify(entries));
+            displayEntries();
+            document.getElementById('user_form').reset();
+        }
     }
-}
 
-function display() {
-    let entries = JSON.parse(localStorage.getItem('userEntries')) || [];
-    let tableBody = document.querySelector('#userentries tbody');
-    tableBody.innerHTML = '';
+    function display() {
+        let entries = JSON.parse(localStorage.getItem('userEntries')) || [];
+        let tableBody = document.querySelector('#userentries tbody');
+        tableBody.innerHTML = '';
 
-    entries.forEach(entry => {
-        const row = `<tr>
-            <td>${entry.Name}</td>
-            <td>${entry.email}</td>
-            <td>${entry.password}</td>
-            <td>${entry.dob}</td>
-            <td>${entry.acceptTerms ? 'Yes' : 'No'}</td>
-        </tr>`;
-        tableBody.innerHTML += row;
-    });
-}
+        entries.forEach(entry => {
+            const row = `<tr>
+                <td>${entry.Name}</td>
+                <td>${entry.email}</td>
+                <td>${entry.password}</td>
+                <td>${entry.dob}</td>
+                <td>${entry.acceptTerms ? 'Yes' : 'No'}</td>
+            </tr>`;
+            tableBody.innerHTML += row;
+        });
+    }
 
-document.getElementById('userform').addEventListener('submit', saveUserForm);
-display();
+    document.getElementById('user_form').addEventListener('submit', saveUserForm);
+    display();
