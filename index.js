805b11f90e9f24e7478e6c8a31ca63ce29c48e58
yatinvarma <151534004+yatinvarma@users.noreply.github.com>
2023-11-20 23:42:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-    function saveUserForm(event) {
+ function saveuserForm(event) {
         event.preventDefault();
+
         const Name = document.getElementById('name').value;
         const email = document.getElementById('email').value;
         const password = document.getElementById('password').value;
         const dob = document.getElementById('dob').value;
         const acceptTerms = document.getElementById('acceptTerms').checked;
+
         var currentYear = new Date().getFullYear();
         var birthYear = dob.split("-");
         let year = birthYear[0];
@@ -12,7 +14,7 @@
 
         if (age < 18 || age > 55) {
             document.getElementById('dob').style.border = '1px solid red';
-            alert("Age must be between 18 and 55");
+            alert("Age must be above 18 and below 55");
         } else {
             document.getElementById('dob').style.border = 'none';
 
@@ -24,32 +26,18 @@
                 acceptTerms
             };
 
-            let entries = JSON.parse(localStorage.getItem('userEntries')) || [];
-            entries.push(entry);
-
-            localStorage.setItem('userEntries', JSON.stringify(entries));
-            setTimeout(displayEntries, 100);
-            
+            display(entry);
             document.getElementById('userform').reset();
         }
     }
 
-    function display() {
-        let entries = JSON.parse(localStorage.getItem('userEntries')) || [];
+    function display(entry) {
         let tableBody = document.querySelector('#userentries tbody');
-        tableBody.innerHTML = '';
-
-        entries.forEach(entry => {
-            const row = `<tr>
-                <td>${entry.Name}</td>
-                <td>${entry.email}</td>
-                <td>${entry.password}</td>
-                <td>${entry.dob}</td>
-                <td>${entry.acceptTerms ? 'Yes' : 'No'}</td>
-            </tr>`;
-            tableBody.innerHTML += row;
-        });
-    }
-
-    document.getElementById('userform').addEventListener('submit', saveUserForm);
-    display();
+        const row = `<tr>
+            <td>${entry.Name}</td>
+            <td>${entry.email}</td>
+            <td>${entry.password}</td>
+            <td>${entry.dob}</td>
+            <td>${entry.acceptTerms ? 'Yes' : 'No'}</td>
+        </tr>`;
+        tableBody.innerHTML = row + tableBody.innerHTML;
