1aec8f5b77da0091e520fcf2cf35f85442603727
yatinvarma <151534004+yatinvarma@users.noreply.github.com>
2023-11-20 19:15:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,45 +1,127 @@
-document.getElementById('registrationForm').addEventListener('submit', function (event) {
-    event.preventDefault();  
-    let dobInput = document.getElementById('dob');
-    let dob = new Date(dobInput.value);
-    let today = new Date();
-    let minDate = new Date();
-    minDate.setFullYear(today.getFullYear() - 55);
-    let maxDate = new Date();
-    maxDate.setFullYear(today.getFullYear() - 18);
+let element = (id) => document.getElementById(id);
 
-    if (dob < minDate || dob > maxDate) {
-        alert('Date of birth must have to be between 18 and 55 years.');
-        return;
+let classes = (classes) => document.getElementsByClassName(classes);
+
+let user_entries = [];
+
+function fillTable(){
+    let obj = localStorage.getItem("user_entries");
+    if(obj){
+        user_entries = JSON.parse(obj);
+    }else{
+        user_entries = [];
     }
-    let formData = {
-        name: document.getElementById('Name').value,
-        email: document.getElementById('Email').value,
-        password: document.getElementById('Password').value,
-        dob: document.getElementById('dob').value,
-        terms: document.getElementById('Terms').checked
-    };
-    let entries = JSON.parse(localStorage.getItem('Entries')) || [];
-    entries.unshift(formData);
-    localStorage.setItem('Entries', JSON.stringify(entries));
-    updateTable();
+    return user_entries;
+}
+user_entries = fillTable();
+
+let username = element("name"),
+  email = element("email"),
+  password = element("password"),
+  tc = element("tc"),
+  dob = element("dob");
+
+let errormsg = classes("errormsg");
+
+let form = element("form");
+
+function verify(elem,message,cnd){
+    if(cnd){
+        elem.style.border = "2px solid red";
+        elem.setCustomValidity(message);
+        elem.reportValidity();
+    }else{
+        elem.style.border = "2px solid green";
+        elem.setCustomValidity('');
+
+    }
+}
+
+function checkDOB(){
+    let age = new Date().getFullYear() - new Date(dob.value).getFullYear();
+    if(age < 18 || age>55){
+        return false;
+    }else{
+        return true;
+    }
+}
+let message_name = "Username must be at least 3 characters long";
+let message_email = "Email must be valid";
+let message_agree = "You must agree to the terms and conditions";
+let message_dob = "You age must be between 18 and 55 to continue";
+
+username.addEventListener("input", (e) => {
+    let cond_name = username.value.length < 3;
+    e.preventDefault();
+    verify(username,message_name,cond_name);
 });
 
-function updateTable() {
-   
-    let tableBody = document.getElementById('UserTable').getElementsByTagName('tbody')[0];
-    let entries = JSON.parse(localStorage.getItem('Entries')) || [];
+email.addEventListener("input", (e) => {
+    let cond_email = !(email.value.includes("@") && email.value.includes("."));
+    e.preventDefault();
+    verify(email,message_email,cond_email);
+});
 
-   
-    tableBody.innerHTML = '';
+dob.addEventListener("input", (e) => {
+    let cond_dob = !checkDOB();
+    e.preventDefault();
+    verify(dob,message_dob,cond_dob);
+});
+tc.addEventListener("input", (e) => {
+    let cond_agree = !tc.checked;
+    e.preventDefault();
+    verify(tc,message_agree,cond_agree);
+});
 
-    
-    entries.forEach(entry => {
-        let row = tableBody.insertRow();
-        Object.values(entry).forEach(value => {
-            let cell = row.insertCell();
-            cell.appendChild(document.createTextNode(value));
-        });
-    });
+function makeObject(){
+    let check = false;
+    if(tc.checked){
+        check = true;
+    }
+    let obj = {
+        name: username.value,
+        email: email.value,
+        password: password.value,
+        dob: dob.value,
+        checked: check
+    }
+    return obj;
 }
-window.addEventListener('load', updateTable);
+
+
+function displayTable(){
+    let table = element("user-table");
+    let entries = user_entries;
+    let str = `<tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Dob</th>
+                    <th>Accepted terms?</th>
+                </tr>\n`;
+    for(let i=0;i<entries.length;i++){
+        str += `<tr>
+                    <td>${entries[i].name}</td>
+                    <td>${entries[i].email}</td>
+                    <td>${entries[i].password}</td>
+                    <td>${entries[i].dob}</td>
+                    <td>${entries[i].checked}</td>
+                </tr>\n`;
+    }
+    table.innerHTML = str;
+}
+
+form.addEventListener("submit", (e) => {
+    let cond_agree= !tc.checked;
+    e.preventDefault();
+    console.log('working');
+    if (!cond_agree) {
+        let obj = makeObject();
+        user_entries.push(obj);
+        localStorage.setItem("user_entries", JSON.stringify(user_entries));
+    }
+    displayTable();
+});
+window.onload = (event) => {
+    displayTable();
+}
